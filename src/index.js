5e74144d41d255a9d4e701fd8cead810c3d3604e
Edgar Gomes <edgargomes.iff@gmail.com>
2022-03-14 19:07:04 -0300

    Implementando debug.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Main from "./components/Main"
-import {BrowserRouter as Router, Route} from "react-router-dom"
+
 
 const rootComponent = document.getElementById('root')
 ReactDOM.render(
-  <Router>
-    <Route path="/" exact render = {() =>(
       <>
         <Main/>
       </>
-    )}/>
-  </Router>
-  , rootComponent);
+      , rootComponent);
 
