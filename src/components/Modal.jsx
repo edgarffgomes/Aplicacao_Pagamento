9d188bbb9dca95adf4e652826dd6a17c52a4be6b
Edgar Gomes <edgargomes.iff@gmail.com>
2022-03-21 08:29:39 -0300

    Máscara adicionada ao input do modal.

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,22 @@
 import React from "react"
 import "./Modal.css"
 const Modal = ({onClose = () =>{}, person}) =>{
+		function mask(e){
+			var pattern = /[0-9 .,]+/g
+			if(pattern.test(e.key) == false){
+				e.preventDefault()
+			}
+			if(e.target.value.length == 0){
+				var insert = (e.target.value/10).toFixed(1)
+				e.target.value = insert.replace('.',',')
+			} else{
+				var insert = e.target.value.replace(',','.')
+				insert = (parseFloat(insert) * 10).toFixed(1)
+				console.log(insert)
+				insert = insert.replace('.',',')		
+				e.target.value = insert
+			}
+		}
 	return(
 		<div className="modal">
 			<div className= "container">
@@ -11,7 +27,7 @@ const Modal = ({onClose = () =>{}, person}) =>{
 						 <button className="header-btn" onClick={onClose}>x</button>
 				</header>
 				<div className="money">
-					<input type="text" placeholder="R$ 0,00"/>
+					<input onKeyPress={(event) => mask(event)} type="text" placeholder="R$ 0,00"/>
 				</div>
 				<div className ="card">
 					<select>
